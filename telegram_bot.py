2af0c926f42f8e514215f137cfbc407865eeaa0b
surya-21 <92663674+surya-21@users.noreply.github.com>
2021-11-21 11:54:47 +0530

    Add files via upload

---
+++ telegram_bot.py
@@ -0,0 +1,44 @@
+import requests
+import json
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+updater = Updater(token='2104108202:AAHKbkfVDNAJwcPZOcdyA3-wleaNfxOfGWs', use_context=True) #Replace TOKEN with your token string
+dispatcher = updater.dispatcher
+
+def hello(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World')
+
+hello_handler = CommandHandler('hello', hello)
+dispatcher.add_handler(hello_handler)
+
+def summary(update, context):
+    response = requests.get('https://api.covid19api.com/summary')
+    if(response.status_code==200):
+        data = response.json()
+        date = data['Date'][:10]
+        ans = f"Covid 19 Summary (as of {date}): \n";
+        
+        for attribute, value in data['Global'].items():
+            if attribute not in ['NewConfirmed', 'NewDeaths', 'NewRecovered']:
+                ans += 'Total ' + attribute[5::].lower() + " : " + str(value) + "\n"
+        
+        print(ans)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=ans)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Error, something went wrong.")
+
+corona_summary_handler = CommandHandler('summary', summary)
+dispatcher.add_handler(corona_summary_handler)
+
+def bye(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text = "bye" )
+
+bye_handler = CommandHandler('bye', bye)
+dispatcher.add_handler(bye_handler)
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+updater.start_polling()
